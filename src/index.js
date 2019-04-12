dc34d8005bc78212dbc957dd1de36df80a467607
jwellwood <joewellwood@gmail.com>
2019-04-12 19:45:04 +0200

    add firebase to project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
+import Firebase, { FirebaseContext } from './firebase';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
