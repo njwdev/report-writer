6a8f255f30effa46a2052d62b9725d7793367b75
njwdev <nwwebd@gmail.com>
2019-04-13 20:46:43 +0200

    added material ui

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,64 @@
 import React from 'react';
+import './Navigation.css';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 
-const Navigation = () => {
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  appBar: {
+    position: 'relative',
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+});
+
+const LandingTest = props => {
+  const { classes } = props;
+
   return (
-    <div>
-      Navigation
-      <div>
-        <a href="/">Landing</a>
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/signin">Sign In</a>
-        <a href="/signup">Sign Up</a>
-        <a href="/admin">Admin</a>
-      </div>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            <a href="/">Reports Writer</a>
+          </Typography>
+          <Button>
+            <a href="/home">Home</a>
+          </Button>
+          <Button>
+            <a href="/about">About</a>
+          </Button>
+          <Button>
+            <a href="/">Support</a>
+          </Button>
+          <Button color="primary" variant="outlined">
+            <a href="/signin">Admin Login</a>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 };
 
-export default Navigation;
+LandingTest.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LandingTest);
