e6c36ca78daba822cfb3dbe24feaa5a7afe168a0
jwellwood <joewellwood@gmail.com>
2019-04-27 19:34:27 +0200

    add new comment form

--- src/components/Admin/Comments/AddCommentForm.js
+++ src/components/Admin/Comments/AddCommentForm.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
+import { withFirebase } from '../../../firebase';
+
+const INITIAL_STATE = {
+  type: '',
+  comment: '',
+  error: null,
+};
 
 class AddNewComment extends Component {
+  state = {
+    ...INITIAL_STATE,
+  };
+
+  onSubmit = e => {
+    const { type, comment } = this.state;
+    const { firebase, history } = this.props;
+    e.preventDefault();
+    return firebase
+      .comments()
+      .add({
+        type,
+        comment,
+      })
+
+      .then(() => history.push('/admin'))
+      .catch(() => this.setState({ error: true }));
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { type, comment, error } = this.state;
+
+    const isInvalid = comment === '';
+
     return (
       <div>
-        <form>
-          <input type="text" />
-          <button>Submit</button>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" name="comment" onChange={this.onChange} />
+          <select onChange={this.onChange} name="type">
+            <option value="intro"> Intro </option>
+            <option value="positive"> Positive </option>
+            <option value="negative"> Negative </option>
+            <option value="closing"> Closing </option>
+          </select>
+          <button type="submit" disabled={isInvalid}>
+            Submit
+          </button>
         </form>
+        {error ? error : null}
       </div>
     );
   }
 }
 
-export default AddNewComment;
+export default withFirebase(AddNewComment);
