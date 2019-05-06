227d931f5c323d2f67bd945f5b0d440c2041d640
njwdev <nwwebd@gmail.com>
2019-05-06 13:28:56 +0200

    updated add comment form

--- src/components/Admin/Comments/AddComment/NewCommentForm.js
+++ src/components/Admin/Comments/AddComment/NewCommentForm.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import Form from '../../../../utility/Form/Form';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel from '@material-ui/core/InputLabel';
+import SubmitButton from '../../../../utility/SubmitButton/SubmitButton';
 
 const NewCommentForm = props => {
-  const { onSubmit, onChange, isInvalid } = props;
+  const { onSubmit, onChange, type, comment, isInvalid } = props;
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="comment" onChange={onChange} />
-        <select onChange={onChange} name="type">
-          <option value="" disabled>
-            Choose here
-          </option>
-          <option value="intro"> Intro </option>
-          <option value="positive"> Positive </option>
-          <option value="negative"> Negative </option>
-          <option value="closing"> Closing </option>
-        </select>
-        <button type="submit" disabled={isInvalid}>
-          Submit
-        </button>
-      </form>
-    </div>
+    <Form onSubmit={onSubmit} autoComplete="off">
+      <FormControl fullWidth>
+        <InputLabel htmlFor="comment">Add comment</InputLabel>
+        <Input
+          multiline
+          value={comment}
+          name="comment"
+          type="comment"
+          id="comment"
+          onChange={onChange}
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="type-native-helper">Type</InputLabel>
+        <NativeSelect
+          value={type}
+          onChange={onChange}
+          input={<Input name="type" id="type-native-helper" />}
+        >
+          <option value="" />
+          <option value={'intro'}>Intro</option>
+          <option value={'positive'}>Positive</option>
+          <option value={'negative'}>Negative</option>
+          <option value={'closing'}>Closing</option>
+        </NativeSelect>
+        <FormHelperText>
+          Choose which part of the report the comment belongs to
+        </FormHelperText>
+      </FormControl>
+      <SubmitButton disabled={isInvalid}> Submit </SubmitButton>
+    </Form>
   );
 };
 
