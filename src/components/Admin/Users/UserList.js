b1c3c83747b8c865775a81cebbeeebffa99d95fa
jwellwood <joewellwood@gmail.com>
2019-05-08 00:22:59 +0200

    list styling for admin page

--- src/components/Admin/Users/UserList.js
+++ src/components/Admin/Users/UserList.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
+import Checkbox from '@material-ui/core/Checkbox';
+import Divider from '@material-ui/core/Divider';
 
 const UserList = props => {
   const { users } = props;
   return (
     <div>
-      <ul>
+      <List dense>
         {users.map(user => (
-          <li key={user.uid}>
-            <span>
-              <strong>ID:</strong> {user.uid}
-            </span>
-            <span>
-              <strong>E-Mail:</strong> {user.email}
-            </span>
-            <span>
-              <strong>Username:</strong> {user.username}
-            </span>
-          </li>
+          <React.Fragment key={user.uid}>
+            <Paper>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Icon>person</Icon>
+                  </Avatar>
+                </ListItemAvatar>
+
+                <ListItemText primary={user.username} secondary={user.email} />
+
+                <ListItemSecondaryAction>
+                  <Checkbox
+                    // checked={this.state.checkedA}
+                    // onChange={this.handleChange('checkedA')}
+                    value="checkedA"
+                  />
+                  <IconButton aria-label="Delete">
+                    <Icon>delete</Icon>
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </Paper>
+            <Divider />
+          </React.Fragment>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
