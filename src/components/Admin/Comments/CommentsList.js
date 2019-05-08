b0d5c8b2b2c9cbfc447074cf6bed6e762c57cd4b
jwellwood <joewellwood@gmail.com>
2019-05-08 13:34:08 +0200

    update lists

--- src/components/Admin/Comments/CommentsList.js
+++ src/components/Admin/Comments/CommentsList.js
@@ -1,47 +1,20 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
-import Divider from '@material-ui/core/Divider';
+import AdminList from '../../layout/Lists/AdminList';
 
 const CommentsList = props => {
   const { comments } = props;
   return (
-    <div>
-      <List dense>
-        {comments.map(comment => (
-          <React.Fragment key={comment.uid}>
-            <Paper>
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <Icon>label_important</Icon>
-                  </Avatar>
-                </ListItemAvatar>
-
-                <ListItemText
-                  primary={comment.type}
-                  secondary={comment.comment}
-                />
-
-                <ListItemSecondaryAction>
-                  <IconButton aria-label="Delete">
-                    <Icon>edit</Icon>
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            </Paper>
-            <Divider />
-          </React.Fragment>
-        ))}
-      </List>
-    </div>
+    <List dense>
+      {comments.map(comment => (
+        <AdminList
+          key={comment.uid}
+          icon="label_important"
+          primary={comment.type}
+          secondary={comment.comment}
+        />
+      ))}
+    </List>
   );
 };
 
---
+++ src/components/layout/Lists/AdminList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Icon from '@material-ui/core/Icon';
+import Divider from '@material-ui/core/Divider';
+import AdminModal from '../../ui/Modal/AdminModal';
+
+const AdminList = props => {
+  const { icon, primary, secondary, onClick } = props;
+  return (
+    <div>
+      <Paper>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <Icon>{icon}</Icon>
+            </Avatar>
+          </ListItemAvatar>
+
+          <ListItemText primary={primary} secondary={secondary} />
+
+          <ListItemSecondaryAction>
+            <AdminModal />
+          </ListItemSecondaryAction>
+        </ListItem>
+      </Paper>
+      <Divider />
+    </div>
+  );
+};
+
+export default AdminList;
