843b3d6df17867c13220a51cdfd0cc72679962ab
njwdev <nwwebd@gmail.com>
2019-05-27 22:45:12 +0200

    started about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import PageContainer from '../layout/Container/PageContainer';
 import PageHeader from '../layout/Container/PageHeader';
 
 const About = () => (
   <PageContainer>
-    <PageHeader title="About" link="/" />
+    <PageHeader title="About" link="/about" />
+    <div>
+      <h1>Welcome to the about page</h1>
+      <List>
+        <ListItem>
+          <a href="mailto:someone@example.com" target="_top">
+            Send Mail
+          </a>
+        </ListItem>
+      </List>
+    </div>
   </PageContainer>
 );
 
